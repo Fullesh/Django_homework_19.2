df98a5aee991299a84907dab3e83d2811b835b1e
Danila Belov <danilabelov.adm@yandex.ru>
2024-02-25 20:25:33 +0300

    created empty models Product and Cateogory with init settings query_2

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+
+    def __str__(self):
+        pass
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+
+class Category(models.Model):
+
+    def __str__(self):
+        pass
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
